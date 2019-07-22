343e1c2a90dbad0459ea65f3d0a10ead03b081a1
wujun <18668465735@163.com>
2019-07-22 19:27:02 +0800

    限流test

--- src/main/java/com/example/product/controller/TestController.java
+++ src/main/java/com/example/product/controller/TestController.java
@@ -64,4 +64,10 @@ public class TestController {
         logger.info(header);
         return "CCC";
     };
+
+
+    @GetMapping("/hello/rateLimit")
+    public  String rateLimit(){
+        return "UUU";
+    };
 }
